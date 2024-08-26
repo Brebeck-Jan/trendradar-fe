eb609d25924fd712a28f875c2c79046f12a52355
jan.brebeck <Jan.Brebeck98@gmail.com>
2024-08-26 14:43:21 +0200

    move back

--- trendradar-fe/src/router/index.ts
+++ trendradar-fe/src/router/index.ts
@@ -1,35 +1,23 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
-import Home from '@/views/Start.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/cv',
-            name: 'cv',
-            component: () => import('../views/Cv.vue')
-        },
-        {
-            path: '/projects',
-            name: 'projects',
-            component: () => import('../views/Projects.vue')
-        },
-        {
-            path: '/certificates',
-            name: 'certificates',
-            component: () => import('../views/Certificates.vue')
-        },
-        {
-            path: '/contact',
-            name: 'contact',
-            component: () => import('../views/Contact.vue')
-        }
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
